798a6fac3842b9db5223be5eb602cc95586a54c5
Hazem-Ben-Abdelhafidh <morbidvisions@yahoo.com>
2022-09-25 10:13:41 +0100

    creating mongo db connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,9 @@
 import { ApolloServer } from "apollo-server";
 import { readFileSync } from "fs";
+import mongoose from "mongoose";
 import { resolvers } from "./resolvers";
-// Create graphql server
 
+// Create graphql server
 const connectGql = async () => {
   const typeDefs = readFileSync("./src/schema.graphql", { encoding: "utf-8" });
   const server = new ApolloServer({
@@ -14,5 +15,18 @@ const connectGql = async () => {
     console.log("graphql server started");
   } catch (error: any) {
     console.log(error.message);
+  }
+};
+
+// Create mongodb connection
+const connectDB = async () => {
+  const DB_URI = process.env.DATABASE as string;
+  try {
+    await mongoose.connect(DB_URI);
+    console.log("connected to Database");
+  } catch (error: any) {
+    console.log(error.message);
+    // retry connection after 5 seconds
+    setTimeout(connectDB, 5000);
   }
 };
